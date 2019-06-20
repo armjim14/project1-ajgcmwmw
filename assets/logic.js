fef4ff2e16e65cfc4972f6b31fdda106fbeb83b6
armjim14 <ajimenez1497@gmail.com>
2019-06-19 19:33:59 -0700

    another commit

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,18 @@
-$("#submit").on("click", function(){
-    var birthdate = $("form input").val()
+var birthdate = "";
+var items = [];
+
+$("body").on("click", "#submit", function(){
+    birthdate = $("form input").val();
+})
+
+$("#nasaCard").on("click", function(){
     var api = "nzDTlixflJIZcchogN9lZyKGc6qW2V0ElS9qHvAD"
-    var link = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + birthdate + "&end_date=" + birthdate + "&api_key=" + api;
-    // var link2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+birthdate+"&api_key=" + api    
-    var items = [];
+    var link = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + birthdate + "&end_date=" + birthdate + "&api_key=" + api;    
+
     $.ajax({
         url: link,
         method: "GET"
     }).then(function(res){
-        // console.log(res);
-
         for( let i = 0; i < res.element_count; i++ ){
             var info = res.near_earth_objects[birthdate][i];
 
@@ -21,22 +24,21 @@ $("#submit").on("click", function(){
             var speed = Math.ceil(info.close_approach_data[0].relative_velocity.miles_per_hour) + " MPH";
             
             // Miles from earth to sun is 93 Million Miles
-            // var objToItems = {
-            //     length: size,
-            //     missed: miss,
-            //     velocity: speed,
-            // }
-            // console.log(objToItems);
-            items.push(size, miss, speed);
-            // console.log(items);
+            var objToItems = {
+                length: size,
+                missed: miss,
+                velocity: speed,
+            }
+            items.push(objToItems);
         }
-        console.log(items);
+        for ( let i = 0; i < items.length; i++ ){
+            var newitem = items[i]
+            var newTr = $("<tr>");
+            var sizetd = $("<td>").text(newitem.length);
+            var missTd = $("<td>").text(newitem.missed);
+            var speedTd = $("<td>").text(newitem.velocity);
+            newTr.append(sizetd, missTd, speedTd);
+            $("#nasatable").append(newTr);
+        }
     })
-    // $.ajax({
-    //     url: link2,
-    //     method: "GET"
-    // }).then(function(res){
-    //     console.log(res);
-    // })
-    
 })
