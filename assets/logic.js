7d16b37e47c4eb529f835b756d6ea5be4b04c679
armjim14 <ajimenez1497@gmail.com>
2019-06-19 19:04:01 -0700

    ajax works

--- assets/logic.js
+++ assets/logic.js
@@ -1,27 +1,42 @@
-// https://api.nasa.gov/planetary/apod?api_key=nzDTlixflJIZcchogN9lZyKGc6qW2V0ElS9qHvAD
-// https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY
-
 $("#submit").on("click", function(){
     var birthdate = $("form input").val()
     var api = "nzDTlixflJIZcchogN9lZyKGc6qW2V0ElS9qHvAD"
     var link = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + birthdate + "&end_date=" + birthdate + "&api_key=" + api;
-
+    // var link2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+birthdate+"&api_key=" + api    
+    var items = [];
     $.ajax({
         url: link,
         method: "GET"
     }).then(function(res){
-        console.log(res);
-        var info = res.near_earth_objects[birthdate][0];
+        // console.log(res);
 
-        //missed distance
-        var miss = Math.floor(info.close_approach_data[0].miss_distance.miles) + " Miles";
+        for( let i = 0; i < res.element_count; i++ ){
+            var info = res.near_earth_objects[birthdate][i];
 
-        //speed of it
-        var speed = Math.ceil(info.close_approach_data[0].relative_velocity.miles_per_hour) + " MPH";
-
-        // Miles from earth to sun is 93 Million Miles
-        console.log(speed);
-        console.log(miss);
+            var size = info.estimated_diameter.miles.estimated_diameter_max;
+            //missed distance
+            var miss = Math.floor(info.close_approach_data[0].miss_distance.miles) + " Miles";
+            
+            //speed of it
+            var speed = Math.ceil(info.close_approach_data[0].relative_velocity.miles_per_hour) + " MPH";
+            
+            // Miles from earth to sun is 93 Million Miles
+            // var objToItems = {
+            //     length: size,
+            //     missed: miss,
+            //     velocity: speed,
+            // }
+            // console.log(objToItems);
+            items.push(size, miss, speed);
+            // console.log(items);
+        }
+        console.log(items);
     })
-
+    // $.ajax({
+    //     url: link2,
+    //     method: "GET"
+    // }).then(function(res){
+    //     console.log(res);
+    // })
+    
 })
