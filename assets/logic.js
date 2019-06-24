85115c68edc561721ed756e5667c154d2b59dc25
Matthew Winemiller <seidoc@gmail.com>
2019-06-23 19:27:37 -0700

    Added a 'no results' output if there's no data in the JSON response

--- assets/logic.js
+++ assets/logic.js
@@ -108,13 +108,6 @@ function earthquakes(date) {
     $("#ajaxResults").css("display", "block");
     $("#back").css("display", "block");
     $("#reset").css("display", "block");
-    var eqTable = $("<table class='mdl-data-table mdl-js-data-table mdl-cell--12-col'>");
-    var eqTableHeaders = $("<thead><tr><th class='mdl-data-table__cell--non-numeric'>Time</th><th class='mdl-data-table__cell--non-numeric'>Location</th><th class='mdl-data-table__cell--non-numeric'>Magnitude</th><th class='mdl-data-table__cell--non-numeric'>Type</th><th class='mdl-data-table__cell--non-numeric'>URL</th></tr></thead>");
-    var eqTableBody = $("<tbody>");
-
-    $("#ajaxResults").append(eqTable);
-    eqTable.append(eqTableHeaders);
-    eqTable.append(eqTableBody);
 
     var eqURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + birthdate + '&endtime=' + moment(birthdate).add(1, 'days').format('YYYY-MM-DD');
 
@@ -122,20 +115,31 @@ function earthquakes(date) {
         url: eqURL,
         method: 'GET'
     }).then(function (response) {
-        for (var i = 0; i < response.features.length; i++) {
-            if (birthdate == moment(response.features[i].properties.time).format('YYYY-MM-DD')) {
-                var newTR = $("<tr>");
-                var timeTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(moment(response.features[i].properties.time).format('YYYY-MM-DD, hh:mm A'));
-                var locTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(response.features[i].properties.place);
-                var magTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(response.features[i].properties.mag);
-                var typeTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(response.features[i].properties.type);
-                var urlTD = $("<td class='mdl-data-table__cell--non-numeric'>").html("<a href='" + response.features[i].properties.url + "' target='_blank'>" + response.features[i].properties.url + "</a>");
-                newTR.append(timeTD);
-                newTR.append(locTD);
-                newTR.append(magTD);
-                newTR.append(typeTD);
-                newTR.append(urlTD);
-                eqTableBody.append(newTR);
+        if (response.features.length == 0) {
+            $("#ajaxResults").html('<h1>'+ "Sorry, there are no results for " + moment(birthdate).format('MMMM Do, YYYY') + "." + '</h1>');
+        }
+        else {
+            var eqTable = $("<table class='mdl-data-table mdl-js-data-table mdl-cell--12-col'>");
+            var eqTableHeaders = $("<thead><tr><th class='mdl-data-table__cell--non-numeric'>Time</th><th class='mdl-data-table__cell--non-numeric'>Location</th><th class='mdl-data-table__cell--non-numeric'>Magnitude</th><th class='mdl-data-table__cell--non-numeric'>Type</th><th class='mdl-data-table__cell--non-numeric'>URL</th></tr></thead>");
+            var eqTableBody = $("<tbody>");
+            $("#ajaxResults").append(eqTable);
+            eqTable.append(eqTableHeaders);
+            eqTable.append(eqTableBody);
+            for (var i = 0; i < response.features.length; i++) {
+                if (birthdate == moment(response.features[i].properties.time).format('YYYY-MM-DD')) {
+                    var newTR = $("<tr>");
+                    var timeTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(moment(response.features[i].properties.time).format('YYYY-MM-DD, hh:mm A'));
+                    var locTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(response.features[i].properties.place);
+                    var magTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(response.features[i].properties.mag);
+                    var typeTD = $("<td class='mdl-data-table__cell--non-numeric'>").text(response.features[i].properties.type);
+                    var urlTD = $("<td class='mdl-data-table__cell--non-numeric'>").html("<a href='" + response.features[i].properties.url + "' target='_blank'>" + response.features[i].properties.url + "</a>");
+                    newTR.append(timeTD);
+                    newTR.append(locTD);
+                    newTR.append(magTD);
+                    newTR.append(typeTD);
+                    newTR.append(urlTD);
+                    eqTableBody.append(newTR);
+                }
             }
         }
     })
